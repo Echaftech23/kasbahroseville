97f406832876e50c7de4d01733847676e0453623
Echaftech23 <rachidchaf2001@gmail.com>
2024-02-24 17:10:52 +0100

    added crud of Facility entity.

--- app/Http/Controllers/Admin/FacilityController.php
+++ app/Http/Controllers/Admin/FacilityController.php
@@ -13,15 +13,11 @@ class FacilityController extends Controller
      */
     public function index()
     {
-        //
-    }
+        $facilities = Facility::latest()->get();
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
+        return response()->json([
+            'data' =>  $facilities,
+        ], 200);
     }
 
     /**
@@ -29,23 +25,18 @@ class FacilityController extends Controller
      */
     public function store(StoreFacilityRequest $request)
     {
-        //
-    }
+        try {
+            Facility::create($request->validated());
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(Facility $facility)
-    {
-        //
-    }
+            return response()->json([
+                'message' => 'Facility created successfully',
+            ], 200);
+        } catch (\Exception $e) {
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Facility $facility)
-    {
-        //
+            return response()->json([
+                'message' => 'Something went wrong!',
+            ], 500);
+        }
     }
 
     /**
@@ -53,7 +44,21 @@ class FacilityController extends Controller
      */
     public function update(UpdateFacilityRequest $request, Facility $facility)
     {
-        //
+        try {
+
+            abort_if(!$facility, 404, 'Facility not found.');
+
+            $facility->update($request->validated());
+
+            return response()->json([
+                'message' => 'Facility Updated successfully',
+            ], 200);
+        } catch (\Exception $e) {
+
+            return response()->json([
+                'message' => 'Something went wrong!',
+            ], 500);
+        }
     }
 
     /**
@@ -61,6 +66,20 @@ class FacilityController extends Controller
      */
     public function destroy(Facility $facility)
     {
-        //
+        try {
+
+            abort_if(!$facility, 404, 'Facility not found.');
+
+            $facility->delete();
+
+            return response()->json([
+                'message' => 'Facility deleted successfully.',
+            ], 200);
+        } catch (\Exception $e) {
+
+            return response()->json([
+                'message' => 'Something went wrong! Please try again.',
+            ], 500);
+        }
     }
 }
