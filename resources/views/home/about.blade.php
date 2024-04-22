aea3d54413c7b8e2b40e882a2743f2fa948225c5
Echaftech23 <rachidchaf2001@gmail.com>
2024-04-22 12:23:22 +0100

    added about Testimonials.

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -27,4 +27,44 @@
       </div>
     </div>
     <!-- Breadcrumb Area End -->
+
+    <!-- About Us Area Start -->
+    <section class="kasbah-about-us-area section-padding-100-0">
+      <div class="container mx-auto">
+        <div class="flex flex-col lg:flex-row">
+          <div class="lg:w-1/2 pr-0 lg:pr-12">
+            <div
+              class="about-thumbnail mb-100 wow fadeInUp"
+              data-wow-delay="100ms"
+            >
+              <img src="{{asset('img/bg-img/19.jpg')}}" alt="" />
+            </div>
+          </div>
+          <div class="lg:w-1/2 pl-0 lg:pl-10">
+            <!-- Section Heading -->
+            <div class="section-heading wow fadeInUp" data-wow-delay="300ms">
+              <h6>Testimonials</h6>
+              <h2>20 Years Of Experience</h2>
+            </div>
+            <div
+              class="about-content mb-100 wow fadeInUp"
+              data-wow-delay="500ms"
+            >
+              <p>
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia dese mollit anim id est laborum. Sed ut perspiciatis
+                unde omnis iste.
+              </p>
+              <p class="my-4">
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa
+                qui officia dese mollit anim id est laborum. Sed ut perspiciatis
+                unde omnis iste. Lorem Ipsum available.
+              </p>
+              <img src="img/core-img/signature.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!-- About Us Area End -->
 @endsection
