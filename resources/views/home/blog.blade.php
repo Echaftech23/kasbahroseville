37cf8633b97fd5b0155753061409504bf558eca7
Echaftech23 <rachidchaf2001@gmail.com>
2024-04-22 13:45:57 +0100

    added blog events right area.

--- resources/views/home/blog.blade.php
+++ resources/views/home/blog.blade.php
@@ -148,7 +148,148 @@
                     </nav>
                 </div>
 
-                
+                <div class="w-full sm:w-8/12 md:w-6/12 lg:w-4/12">
+                    <div class="kasbah-sidebar-area md:pl-12">
+                        <!-- Newsletter -->
+                        <div class="single-widget-area mb-100">
+                            <div class="newsletter-form">
+                                <h5>Newsletter</h5>
+                                <p>
+                                    Subscribe our newsletter gor get notification new updates.
+                                </p>
+
+                                <form action="#" method="post">
+                                    <input type="email" name="nl-email" id="nlEmail"
+                                        class="form-control w-full my-3 pl-3" placeholder="Enter your email..." />
+                                    <button type="submit" class="btn hero-btn w-full">
+                                        Subscribe
+                                    </button>
+                                </form>
+                            </div>
+                        </div>
+
+                        <!-- Recent Post -->
+                        <div class="single-widget-area mb-100">
+                            <h4 class="widget-title mb-30">Recent News</h4>
+
+                            <!-- Single Recent Post -->
+                            <div class="single-recent-post flex">
+                                <!-- Thumb -->
+                                <div class="post-thumb">
+                                    <a href="single-blog.html"><img src="{{asset('img/blogs/blog-5.jpg')}}" alt="" /></a>
+                                </div>
+                                <!-- Content -->
+                                <div class="post-content">
+                                    <!-- Post Meta -->
+                                    <div class="post-meta">
+                                        <a href="single-blog.html" class="post-author">Jan 01, 2024</a>
+                                        <a href="single-blog.html" class="post-tutorial">Event</a>
+                                    </div>
+                                    <a href="single-blog.html" class="post-title">Marrakech Top Things To Do</a>
+                                </div>
+                            </div>
+
+                            <!-- Single Recent Post -->
+                            <div class="single-recent-post flex">
+                                <!-- Thumb -->
+                                <div class="post-thumb">
+                                    <a href="single-blog.html"><img src="{{asset('img/blogs/blog-4.jpg')}}" alt="" /></a>
+                                </div>
+                                <!-- Content -->
+                                <div class="post-content">
+                                    <!-- Post Meta -->
+                                    <div class="post-meta">
+                                        <a href="single-blog.html" class="post-author">Jan 01, 2024</a>
+                                        <a href="single-blog.html" class="post-tutorial">Event</a>
+                                    </div>
+                                    <a href="single-blog.html" class="post-title">World Heritage Sites in Morocco</a>
+                                </div>
+                            </div>
+
+                            <!-- Single Recent Post -->
+                            <div class="single-recent-post flex">
+                                <!-- Thumb -->
+                                <div class="post-thumb">
+                                    <a href="single-blog.html"><img src="{{asset("img/blogs/berber-people-2.jpg")}}"
+                                            alt="" /></a>
+                                </div>
+                                <!-- Content -->
+                                <div class="post-content">
+                                    <!-- Post Meta -->
+                                    <div class="post-meta">
+                                        <a href="single-blog.html" class="post-author">Jan 01, 2024</a>
+                                        <a href="single-blog.html" class="post-tutorial">Event</a>
+                                    </div>
+                                    <a href="single-blog.html" class="post-title">Erg Chigaga: Into The Wild Sahara</a>
+                                </div>
+                            </div>
+
+                            <!-- Single Recent Post -->
+                            <div class="single-recent-post flex">
+                                <!-- Thumb -->
+                                <div class="post-thumb">
+                                    <a href="single-blog.html">
+                                        <img src="{{asset('img/blogs/berber-people.jpg')}}" alt="" />
+                                    </a>
+                                </div>
+                                <!-- Content -->
+                                <div class="post-content">
+                                    <!-- Post Meta -->
+                                    <div class="post-meta">
+                                        <a href="single-blog.html" class="post-author">Jan 01, 2024</a>
+                                        <a href="single-blog.html" class="post-tutorial">Event</a>
+                                    </div>
+                                    <a href="single-blog.html" class="post-title">Who Are The Morocco Berbers?</a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- Popular Tags : Edited style from : mb-100 to style="margin-bottom: 200px" -->
+                        <div class="single-widget-area clearfix" style="margin-bottom: 200px">
+                            <h4 class="widget-title mb-30">Tags</h4>
+                            <!-- Popular Tags -->
+                            <ul class="popular-tags">
+                                <li><a href="#">Bed,</a></li>
+                                <li><a href="#">Hotel,</a></li>
+                                <li><a href="#">Travel,</a></li>
+                                <li><a href="#">Restaurant,</a></li>
+                                <li><a href="#">Sport,</a></li>
+                                <li><a href="#">Trip,</a></li>
+                                <li><a href="#">Music,</a></li>
+                                <li><a href="#">Holiday,</a></li>
+                                <li><a href="#">Tourist,</a></li>
+                                <li><a href="#">Foody,</a></li>
+                                <li><a href="#">Resorts.</a></li>
+                            </ul>
+                        </div>
+
+                        <!-- Instagram -->
+                        <div class="single-widget-area mb-100 clearfix">
+                            <h4 class="widget-title mb-30">Instagram</h4>
+                            <!-- Instagram Feeds -->
+                            <ul class="instagram-feeds">
+                                <li>
+                                    <a href="#"><img src="{{asset('img/blogs/berber-people-2.jpg')}}" alt="" /></a>
+                                </li>
+                                <li>
+                                    <a href="#"><img src="{{{asset('img/blogs/blog-3.jpg')}}}" alt="" /></a>
+                                </li>
+                                <li>
+                                    <a href="#"><img src="{{asset('img/blogs/blog-4.jpg')}}" alt="" /></a>
+                                </li>
+                                <li>
+                                    <a href="#"><img src="{{asset('img/blogs/blog-5.jpg')}}" alt="" /></a>
+                                </li>
+                                <li>
+                                    <a href="#"><img src="{{asset('img/blogs/insta-post-2.jpg')}}" alt="" /></a>
+                                </li>
+                                <li>
+                                    <a href="#"><img src="{{asset('img/blogs/insta-post-3.jpg')}}" alt="" /></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
