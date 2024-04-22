03b88de002bd5adc80c17f33d32ee0dfc3dc0584
Echaftech23 <rachidchaf2001@gmail.com>
2024-04-22 12:35:45 +0100

    added about vedio description section.

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,70 +1,81 @@
 @extends('layouts.home.app')
 
 @section('contents')
-<!-- Breadcrumb Area Start -->
-    <div
-      class="breadcrumb-area bg-img bg-overlay jarallax"
-      style="background-image: url({{asset('img/bg-img/16.jpg')}})"
-    >
-      <div class="container mx-auto h-full">
-        <div class="flex h-full items-center">
-          <div class="w-full">
-            <div class="breadcrumb-content text-center">
-              <h2 class="page-title pb-2">About Us</h2>
-              <nav aria-label="breadcrumb">
-                <ol class="breadcrumb flex justify-center">
-                  <li class="breadcrumb-item">
-                    <a href="{{ route('home') }}">Home</a>
-                  </li>
-                  <li class="breadcrumb-item active" aria-current="page">
-                    About Us
-                  </li>
-                </ol>
-              </nav>
+    <!-- Breadcrumb Area Start -->
+    <div class="breadcrumb-area bg-img bg-overlay jarallax" style="background-image: url({{ asset('img/bg-img/16.jpg') }})">
+        <div class="container mx-auto h-full">
+            <div class="flex h-full items-center">
+                <div class="w-full">
+                    <div class="breadcrumb-content text-center">
+                        <h2 class="page-title pb-2">About Us</h2>
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb flex justify-center">
+                                <li class="breadcrumb-item">
+                                    <a href="{{ route('home') }}">Home</a>
+                                </li>
+                                <li class="breadcrumb-item active" aria-current="page">
+                                    About Us
+                                </li>
+                            </ol>
+                        </nav>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
     <!-- Breadcrumb Area End -->
 
     <!-- About Us Area Start -->
     <section class="kasbah-about-us-area section-padding-100-0">
-      <div class="container mx-auto">
-        <div class="flex flex-col lg:flex-row">
-          <div class="lg:w-1/2 pr-0 lg:pr-12">
-            <div
-              class="about-thumbnail mb-100 wow fadeInUp"
-              data-wow-delay="100ms"
-            >
-              <img src="{{asset('img/bg-img/19.jpg')}}" alt="" />
+        <div class="container mx-auto">
+            <div class="flex flex-col lg:flex-row">
+                <div class="lg:w-1/2 pr-0 lg:pr-12">
+                    <div class="about-thumbnail mb-100 wow fadeInUp" data-wow-delay="100ms">
+                        <img src="{{ asset('img/thumbnail/1686946853_DSC_6809.jpg') }}" alt="" />
+                    </div>
+                </div>
+                <div class="lg:w-1/2 pl-0 lg:pl-10">
+                    <!-- Section Heading -->
+                    <div class="section-heading wow fadeInUp" data-wow-delay="300ms">
+                        <h6>Testimonials</h6>
+                        <h2>10 Years Of Experience</h2>
+                    </div>
+                    <div class="about-content mb-100 wow fadeInUp" data-wow-delay="500ms">
+                        <p>
+                            Kasbah RoseVille is a captivating and opulent kasbah
+                            nestled in the heart of the picturesque Kalaat Magouna valleys.
+                            This magnificent kasbah is a true gem that exudes luxury, elegance,
+                        </p>
+                        <p class="my-4">
+                            and charm in
+                            every corner, making it an unforgettable destination for discerning
+                            travelers seeking an authentic Moroccan experience.
+                        </p>
+                    </div>
+                </div>
             </div>
-          </div>
-          <div class="lg:w-1/2 pl-0 lg:pl-10">
-            <!-- Section Heading -->
-            <div class="section-heading wow fadeInUp" data-wow-delay="300ms">
-              <h6>Testimonials</h6>
-              <h2>20 Years Of Experience</h2>
-            </div>
-            <div
-              class="about-content mb-100 wow fadeInUp"
-              data-wow-delay="500ms"
-            >
-              <p>
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                qui officia dese mollit anim id est laborum. Sed ut perspiciatis
-                unde omnis iste.
-              </p>
-              <p class="my-4">
-                Excepteur sint occaecat cupidatat non proident, sunt in culpa
-                qui officia dese mollit anim id est laborum. Sed ut perspiciatis
-                unde omnis iste. Lorem Ipsum available.
-              </p>
-              <img src="img/core-img/signature.png" alt="" />
-            </div>
-          </div>
         </div>
-      </div>
     </section>
     <!-- About Us Area End -->
+
+    <!-- Video Area Start -->
+    <div class="kasbah--video--area bg-img bg-overlay jarallax section-padding-0-100"
+        style="background-image: url({{asset('img/bg-img/20.jpg')}})">
+        <div class="container mx-auto h-full">
+            <div class="flex h-full items-center justify-center">
+                <div class="w-full md:w-1/2">
+                    <!-- Section Heading -->
+                    <div class="section-heading text-center white wow fadeInUp" data-wow-delay="100ms">
+                        <h6>Ultimate Solutions</h6>
+                        <h2>Our Kasbah &amp; Room</h2>
+                    </div>
+                    <div class="video-content-area mt-100 wow fadeInUp" data-wow-delay="500ms">
+                        <a href="https://www.youtube.com/watch?v=UtrZEJUatC4" class="video-play-btn"><i
+                                class="fa fa-play"></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Video Area End -->
 @endsection
